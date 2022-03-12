b487a6b4f259c93ee70c6754076e461bf84e78bd
amirphl <amirphl4@gmail.com>
2022-03-13 03:12:31 +0330

    feat: Configure database connector

---
+++ database-config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+// SetupDB is SetupDB :))
+func SetupDB() *gorm.DB {
+	errEnv := godotenv.Load()
+
+	if errEnv != nil {
+		panic("failed to load env var.")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPass, dbName, dbPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect to the database.")
+	}
+
+	return db
+}
+
+// CloseDB is CloseDB :))
+func CloseDB(db *grom.DB) {
+	db2, err := db.DB()
+
+	if err != nil {
+		panic("failed to close the database connection.")
+	}
+
+	db2.Close()
+}
