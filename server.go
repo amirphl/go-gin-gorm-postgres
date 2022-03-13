bffb9f44e4325df5ffc52aa0744e558dd2e2bd6c
amirphl <amirphl4@gmail.com>
2022-03-13 12:14:01 +0330

    feat: Append routes of login and register

--- server.go
+++ server.go
@@ -1,14 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/amirphl/go-gin-gorm-postgres/config"
+	"github.com/amirphl/go-gin-gorm-postgres/controller"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDB()
+	authController controller.AuthController = controller.CreateAuthController()
+)
 
 func main() {
+	defer config.CloseDB(db)
+
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world",
-		})
-	})
+
+	authRoutes := r.Group("api/v1/auth")
+	// This is just a block!
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 
 	r.Run()
 }
